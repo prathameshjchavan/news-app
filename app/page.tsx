f9c49f60dafa537d1d55f8d6580d76a3af5a54a4
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-03-23 10:31:58 +0530

    building fetch news function

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+import { categories } from "@/constants";
+import { fetchNews } from "@/lib/fetchNews";
+
 const Homepage = async () => {
+	// fetch the news data
+	const data: NewsResponse = await fetchNews(categories.join(","));
+
 	return <div>Homepage</div>;
 };
 
---
+++ lib/fetchNews.ts
@@ -0,0 +1,10 @@
+export const fetchNews = async (
+	category?: Category | string,
+	keywords?: string,
+	isDynamic?: boolean
+) => {
+	// GraphQL query
+	// Fetch function with Next.js 13 caching
+	// Sort function by images vs not images present
+	// return res
+};
