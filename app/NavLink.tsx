cb780d3a9d188dcfc0ffd638810d6e0dd01c4974
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-03-23 10:03:11 +0530

    building nav links

--- app/NavLink.tsx
+++ app/NavLink.tsx
@@ -5,9 +5,15 @@ type Props = {
 	isActive: boolean;
 };
 
-function NavLink({ category }: Props) {
+function NavLink({ category, isActive }: Props) {
 	return (
-		<Link href={`/news/${category}`} className="navLink">
+		<Link
+			href={`/news/${category}`}
+			className={`navLink ${
+				isActive &&
+				"underline decoration-orange-400 underline-offset-4 font-bold text-lg"
+			}`}
+		>
 			{category}
 		</Link>
 	);
