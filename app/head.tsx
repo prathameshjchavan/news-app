781a9840dcc55a0a5f25e2bbc5c6612343dd640e
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-03-23 09:54:42 +0530

    initial commit

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+export default function Head() {
+	return (
+		<Fragment>
+			<title>NEWS</title>
+			<meta content="width=device-width, initial-scale=1" name="viewport" />
+			<link rel="icon" href="/favicon.ico" />
+		</Fragment>
+	);
+}
